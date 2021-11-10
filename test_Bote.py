fc5ea017ca3e9a6cb8185b8efa0fe32ac1293f3a
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2021-11-10 18:08:27 -0400

    bote constructor

--- test_Bote.py
+++ test_Bote.py
@@ -13,9 +13,7 @@ pacifico1Img = pygame.image.load('imagenes/pacifico1.png')
 verdugo1Img = pygame.image.load('imagenes/verdugo1.png')
 
 
-class TestBote(unittest.TestCase):
-
-    print("--------tests valores de resultado conocido-----------")
+class TestBoteCtr(unittest.TestCase):
 
     def test_BoteEnX_with_three_positives(self):
         self.assertEqual(Bote(157, 478, 2, pacifico1Img,
@@ -24,6 +22,27 @@ class TestBote(unittest.TestCase):
                               verdugo1Img, ventana).x, 1)
         self.assertEqual(Bote(123456987965465432132138464831381384313546384344648646, 478, 2, pacifico1Img,
                               verdugo1Img, ventana).x, 123456987965465432132138464831381384313546384344648646)
-
-#if __name__ == '__main__':
-#    unittest.main()
+    def test_BoteEnY_with_three_positives(self):
+        self.assertEqual(Bote(1, 1, 2, pacifico1Img,
+                              verdugo1Img, ventana).y, 1)
+        self.assertEqual(Bote(1, 478, 2, pacifico1Img,
+                              verdugo1Img, ventana).y, 478)
+        self.assertEqual(Bote(1, 123456987965465432132138464831381384313546384344648646, 2, pacifico1Img,
+                              verdugo1Img, ventana).y, 123456987965465432132138464831381384313546384344648646)
+    
+    def test_BoteEnPos(self):
+        self.assertEqual(Bote(1, 1, 4, pacifico1Img,
+                            verdugo1Img, ventana).pos, 4)
+    def test_Bote_imagen1(self):
+        self.assertEqual(Bote(1, 1, 2, pacifico1Img,
+                            verdugo1Img, ventana).imagen1, pacifico1Img)
+       
+    def test_Bote_imagen2(self):
+        self.assertEqual(Bote(1, 1, 2, pacifico1Img,
+                            verdugo1Img, ventana).imagen2, verdugo1Img)
+    
+    
+# class TestBoteMover(unittest.TestCase):
+#     bote = Bote(157, 478, 2, pacifico1Img,verdugo1Img, ventana)
+#     def test_mover(self):
+#         self.assertEqual()
